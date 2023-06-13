b96029d6965f1fa0165804028b1bb864408a736f
Samuel Rodriguez <sam.rod.719@gmail.com>
2023-06-13 15:50:00 -0700

    add to comments to HTML template through JS

--- script.js
+++ script.js
@@ -1,7 +1,78 @@
-// -- COUNTER COMPONENT --
+// -- GLOBAL --
+const MAX_CHARS = 150;
+
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
+const formEl = document.querySelector('.form');
+const feedbackListEl = document.querySelector('.feedbacks');
+const submitBtnEl = document.querySelector('.submit-btn__text');
 
+
+// -- COUNTER COMPONENT --
 textareaEl.addEventListener('input', () => {
-    counterEl.textContent = 150 - textareaEl.value.length;
+    counterEl.textContent = MAX_CHARS - textareaEl.value.length;
+});
+
+
+// -- FORM COMPONENT -- 
+formEl.addEventListener('submit', event => {
+    // prevent default browser action
+    event.preventDefault();
+
+    //get text from textarea
+    const text = textareaEl.value;
+
+    // validate text (e.g. check if "#" is present, is text long enough?)
+    if (text.includes('#') && text.length >= 5) {
+        textareaEl.classList.add('form--valid');
+
+        setTimeout(() => {
+            textareaEl.classList.remove('form--valid');
+        }, 2000);
+    } else {
+        textareaEl.classList.add('form--invalid');
+
+        setTimeout(() => {
+            textareaEl.classList.remove('form--invalid');
+        }, 2000);
+
+        // focus textarea
+        textareaEl.focus();
+        return;
+    }
+
+    // we have a valid sumbition, now we need to extract info from it
+    const hashtag = text.split(' ').find(word => word.includes('#')).trim();
+    const company = hashtag.substring(1);
+    const badgeLetter = company.substring(0, 1).toUpperCase();
+    const upvoteCount = 0;
+    const daysAgo = 0;
+
+    // new feedback item HTML
+    const feedbackHTML = `
+        <li class="feedback">
+            <button class="upvote">
+                <i class="fa-solid fa-caret-up upvote__icon"></i>
+                <span class="upvote__count">${upvoteCount}</span>
+            </button>
+            <section class="feedback__badge">
+                <p class="feedback__letter">${badgeLetter}</p>
+            </section>
+            <div class="feedback__content">
+                <p class="feedback__company">${company}</p>
+                <p class="feedback__text">${text}</p>
+            </div>
+            <p class="feedback__date">${daysAgo === 0 ? 'NEW' : `${daysAgo}d`}</p>
+        </li>
+    `;
+
+    // insert feedback item into our template
+    feedbackListEl.insertAdjacentHTML('beforeend', feedbackHTML);
+
+    // rest our form
+    textareaEl.value = '';
+
+    submitBtnEl.blur();
+
+    counterEl.textContent = MAX_CHARS;
 });
